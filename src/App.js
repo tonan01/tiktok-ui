0a8ecd350cc7e4854c9fddae505fdc9683b7ea4e
Bao Pham Gia <paopaokk321@gmail.com>
2023-12-12 18:31:17 +0700

    [Router]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { privateRoutes, publicRoutes } from '~/routes';
+
 function App() {
     return (
-        <div className="App">
-            <h1>react app</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={<Page />} />;
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
