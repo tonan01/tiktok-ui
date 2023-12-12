0684fa31f0f511fbfe1ef2c5557167b17c2821dd
Bao Pham Gia <paopaokk321@gmail.com>
2023-12-12 16:26:06 +0700

    [Library add]  abel-plugin-module-resolver

---
+++ src/MyAlert.js
@@ -0,0 +1,4 @@
+const myAlert = () => {
+  alert("HIHI");
+};
+export default myAlert;
---
+++ src/componemts/Button/index.js
@@ -0,0 +1,7 @@
+import myAlert from "~/MyAlert";
+
+function Button() {
+  return <button onClick={myAlert}>Click me!</button>;
+}
+
+export default Button;
