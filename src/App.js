2e07bfe0e51226abb1ee4d609a3eefc4974f20a3
Bao Pham Gia <paopaokk321@gmail.com>
2023-12-12 16:29:53 +0700

    chỉ 1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.1231
+          Edit <code>src/App.js</code> and save to reload.1231ffff
         </p>
         <a
           className="App-link"
