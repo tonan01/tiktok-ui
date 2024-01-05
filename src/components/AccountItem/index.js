1469eddb9ae8631fdc97960d7f3a07e064267f95
Bao Pham Gia <paopaokk321@gmail.com>
2024-01-05 17:12:25 +0700

    [Header ] -button

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/0750723e7229e415be4cc278fef5caca~c5_100x100.jpeg?lk3s=a5d48078&x-expires=1704614400&x-signature=iPOFfN4EJkJGNKoJaWTcwRcZMz8%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon  className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>Nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
